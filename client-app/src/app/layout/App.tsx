b37c95c1c80af40b11447f3f7fb4507f75e44c6a
kyoung90 <kenneth.young90@gmail.com>
2019-09-21 22:39:20 -0400

    Changed from class component to functional component. Future of react.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,43 +1,33 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import { Header, Icon, List} from 'semantic-ui-react'
-import './styles.css';
-import { IActivity } from '../models/Activity';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./styles.css";
+import { IActivity } from "../models/Activity";
 
-interface IState{
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState>{
-  readonly state: IState = {
-    activities: []
-  }
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
 
-  componentDidMount(){
-    axios.get<IActivity[]>("http://localhost:5000/api/activities")
-      .then((response) => {
-        this.setState({
-          activities: response.data
-        })
-      })
-    
-  }
-
-  render(){
-    let fixed : boolean = false
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}
-        </List>
-      </div>
-    );
-  }
-  
-}
+  return (
+    <List>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </List>
+  );
+};
 
 export default App;
