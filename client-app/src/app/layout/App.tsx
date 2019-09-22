32334810572ad4bd7ab59bbe21dd3d864951e462
kyoung90 <kenneth.young90@gmail.com>
2019-09-21 21:58:09 -0400

    Added Interface to Activities in react

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,14 +2,19 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import { Header, Icon, List} from 'semantic-ui-react'
 import './styles.css';
+import { IActivity } from '../models/Activity';
 
-class App extends Component{
-  state = {
+interface IState{
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState>{
+  readonly state: IState = {
     activities: []
   }
 
   componentDidMount(){
-    axios.get("http://localhost:5000/api/activities")
+    axios.get<IActivity[]>("http://localhost:5000/api/activities")
       .then((response) => {
         this.setState({
           activities: response.data
@@ -27,7 +32,7 @@ class App extends Component{
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.activities.map((activity: any) => <List.Item key={activity.id}>{activity.title}</List.Item>)}
+          {this.state.activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}
         </List>
       </div>
     );
